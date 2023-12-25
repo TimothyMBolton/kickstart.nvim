c4d73c5aaa8f4a9c99658c1305cc25b602b5b353
Timothy Bolton <timothybolton@Timothys-MBP.localdomain>
2023-12-25 17:09:41 +1100

    Initial commit + add new plugins + one plugin per file organisation

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+
+return {'akinsho/toggleterm.nvim', version = "*",
+  config = function()
+    require('toggleterm').setup({
+      open_mapping = [[<M-/>]],
+      direction = "float",
+      insert_mappings = true,
+    })
+  end,
+}
+
